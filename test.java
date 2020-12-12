5833d648b9d75b8e25f84dbb102527c19fd938a6
lynntao1 <66797093+lynntao1@users.noreply.github.com>
2020-12-12 15:05:54 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,21 +1,25 @@
 public class test{
 
-    int key = 0;
-     int pass = 0000;
+   int key = 0;
+   int pass = 0000;
 
-    public test(int k){
-        key = k;
-    }
+   public test(int k){
+      key = k;
+   }
 
-    public int getKey(int p){
-        if(p==pass) {
-            return key;
-        }
-        public in setPass(int k, int p){
-            if (k == key)
-                pass = p;
-        }
-
-        return 1985000;
-    }
+   public int getKey(int p)
+   {
+      if(p==pass) {
+         return key;
+      }
+   }
+   public int setPass(int k, int p)
+   {
+      {
+         if (k == key)
+            pass = p;
+      }
+      return 1985000;
+   }
+  
 }
